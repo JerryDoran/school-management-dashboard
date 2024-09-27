5f7c55303ce1cdd471d32b1aec41c5b781539f39
Jerome Doran <disneyfan9184@gmail.com>
2024-09-27 14:53:41 -0400

    initial project setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 const Homepage = () => {
-  return (
-    <div className=''>Homepage</div>
-  )
-}
+  return <div className=''>School Management App</div>;
+};
 
-export default Homepage
+export default Homepage;
